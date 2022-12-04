ac5d049329f1b506b7478599bca7771e680dd476
Gwen Kornak <cool@coolspages.com>
2022-12-03 21:27:10 -0800

    Day 4. im not talking about this one

---
+++ day4.cpp
@@ -0,0 +1,61 @@
+
+using namespace std;
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <set>
+
+void tokenize(std::string const &str, const char delim,
+              std::vector<std::string> &out)
+{
+  size_t start;
+  size_t end = 0;
+
+  while ((start = str.find_first_not_of(delim, end)) != std::string::npos)
+  {
+    end = str.find(delim, start);
+    out.push_back(str.substr(start, end - start));
+  }
+}
+
+int main() {
+  cout << "Day 4" << endl;
+  ifstream file(R"(C:\Users\gwen\Documents\2_Programming\Advent of Code\Advent of Code 2022\inputs\day4.txt)");
+  string str;
+
+  int ans1 = 0;
+  int ans2 = 0;
+
+  while (getline(file, str)) {
+    string line = str;
+
+    vector<string> out;
+    tokenize(line, ',', out);
+
+    vector<int> container1;
+    vector<int> container2;
+
+    vector<string> range1;
+    vector<string> range2;
+    tokenize(out[0], '-', range1);
+    tokenize(out[1], '-', range2);
+
+    if (stoi(range1[0]) <= stoi(range2[0]) && stoi(range1[1]) >= stoi(range2[1])) {
+      ans1++;
+    } else if (stoi(range2[0]) <= stoi(range1[0]) && stoi(range2[1]) >= stoi(range1[1])) {
+      ans1++;
+    }
+
+    if (stoi(range1[0]) <= stoi(range2[0]) && stoi(range2[0]) <= stoi(range1[1])) {
+      ans2++;
+    } else if (stoi(range2[0]) <= stoi(range1[0]) && stoi(range1[0]) <= stoi(range2[1])) {
+      ans2++;
+    }
+  }
+
+  cout << "Part 1: " << ans1 << endl;
+  cout << "Part 2: " << ans2 << endl;
+  return 0;
+}
